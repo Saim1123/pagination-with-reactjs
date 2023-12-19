eb65cd9e52d8e6175075f75c0f24865be57f5dde
saim1123 <saimtahir3211@gmail.com>
2023-12-19 22:24:32 +0500

    builded the pagination component and adding the functionalities and styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import Posts from "./components/Posts";
+import Pagination from "./components/Pagination";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -26,10 +27,23 @@ function App() {
     fetchPosts();
   }, []);
 
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+  const paginate = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div className="container mt-5">
       <h1 className="text-primary mb-3">My Blog</h1>
-      <Posts posts={posts} loading={loading} />
+      <Posts posts={currentPosts} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
